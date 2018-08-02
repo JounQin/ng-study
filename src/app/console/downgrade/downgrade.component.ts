71bb1de089956959d6f41f155b4b19f1a7f3d174
Jiaren Liu <jrliu@alauda.io>
2018-08-02 21:29:02 +0800

    refactor: move synced to outside

--- src/app/console/downgrade/downgrade.component.ts
+++ src/app/console/downgrade/downgrade.component.ts
@@ -2,21 +2,21 @@ import { Component, ElementRef, OnInit } from '@angular/core'
 import { setUpLocationSync } from '@angular/router/upgrade'
 import { UpgradeModule } from '@angular/upgrade/static'
 
+let synced: boolean
+
 @Component({
   template: '<div ng-view></div>',
 })
 export class DowngradeComponent implements OnInit {
-  synced: boolean
-
   constructor(private upgrade: UpgradeModule, private elRef: ElementRef) {}
 
   ngOnInit() {
     this.upgrade.bootstrap(this.elRef.nativeElement, ['app'], {
       strictDi: true,
     })
-    if (!this.synced) {
+    if (!synced) {
       setUpLocationSync(this.upgrade)
-      this.synced = true
+      synced = true
     }
   }
 }
