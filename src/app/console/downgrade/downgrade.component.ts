954f2b692f14d6b1bed259e5a1cc0cd4ef99f4ff
Jiaren Liu <jrliu@alauda.io>
2018-08-03 20:41:27 +0800

    fix: setUpLocationSync of @angular/router/upgrade

--- src/app/console/downgrade/downgrade.component.ts
+++ src/app/console/downgrade/downgrade.component.ts
@@ -1,6 +1,33 @@
+import { LocationStrategy } from '@angular/common'
 import { Component, ElementRef, OnInit } from '@angular/core'
-import { setUpLocationSync } from '@angular/router/upgrade'
+import { Router } from '@angular/router'
 import { UpgradeModule } from '@angular/upgrade/static'
+
+export const setUpLocationSync = (ngUpgrade: UpgradeModule) => {
+  if (!ngUpgrade.$injector) {
+    throw new Error(`
+        RouterUpgradeInitializer can be used only after UpgradeModule.bootstrap has been called.
+        Remove RouterUpgradeInitializer and call setUpLocationSync after UpgradeModule.bootstrap.
+      `)
+  }
+
+  const ngInjector = ngUpgrade.injector
+  const router = ngUpgrade.injector.get(Router)
+  const locationStrategy: LocationStrategy = ngInjector.get(LocationStrategy)
+  const baseHref = locationStrategy.getBaseHref().replace(/\/+$/, '') // remove last slash char to ensure navigated url bellow starting with slash
+  const url = document.createElement('a')
+
+  ngUpgrade.$injector
+    .get('$rootScope')
+    .$on('$locationChangeStart', (_: any, next: string, __: string) => {
+      url.href = next
+      let { pathname } = url
+      if (baseHref && !pathname.indexOf(baseHref)) {
+        pathname = pathname.slice(baseHref.length)
+      }
+      router.navigateByUrl(pathname + url.search + url.hash)
+    })
+}
 
 let synced: boolean
 
