4597672037892a8156b0c58f92b9aa8e6b7ca581
Jiaren Liu <jrliu@alauda.io>
2018-08-15 12:16:10 +0800

    bump dependencies, destroy previous Angular.js App before bootstrapping

--- src/app/console/downgrade/downgrade.component.ts
+++ src/app/console/downgrade/downgrade.component.ts
@@ -1,67 +1,30 @@
-import { Location } from '@angular/common'
-import { Component, ElementRef, OnInit } from '@angular/core'
-import { Router } from '@angular/router'
+import { Component, ElementRef, OnDestroy, OnInit } from '@angular/core'
+import { setUpLocationSync } from '@angular/router/upgrade'
 import { UpgradeModule } from '@angular/upgrade/static'
-
-let anchor: HTMLAnchorElement | undefined
-
-function resolveUrl(
-  url: string,
-): {
-  pathname: string
-  search: string
-  hash: string
-} {
-  if (!anchor) {
-    anchor = document.createElement('a')
-  }
-
-  anchor.setAttribute('href', url)
-  anchor.setAttribute('href', anchor.href)
-
-  return {
-    // IE does not start `pathname` with `/` like other browsers.
-    pathname: `/${anchor.pathname.replace(/^\//, '')}`,
-    search: anchor.search,
-    hash: anchor.hash,
-  }
-}
-
-export function setUpLocationSync(ngUpgrade: UpgradeModule) {
-  if (!ngUpgrade.$injector) {
-    throw new Error(`
-        RouterUpgradeInitializer can be used only after UpgradeModule.bootstrap has been called.
-        Remove RouterUpgradeInitializer and call setUpLocationSync after UpgradeModule.bootstrap.
-      `)
-  }
-
-  const router: Router = ngUpgrade.injector.get(Router)
-  const location: Location = ngUpgrade.injector.get(Location)
-
-  ngUpgrade.$injector
-    .get('$rootScope')
-    .$on('$locationChangeStart', (_: any, next: string, __: string) => {
-      const url = resolveUrl(next)
-      const path = location.normalize(url.pathname)
-      router.navigateByUrl(path + url.search + url.hash)
-    })
-}
-
-let synced: boolean
 
 @Component({
   template: '<div ui-view></div>',
 })
-export class DowngradeComponent implements OnInit {
+export class DowngradeComponent implements OnInit, OnDestroy {
   constructor(private upgrade: UpgradeModule, private elRef: ElementRef) {}
 
+  private destroyAngularJsApp() {
+    if (!this.upgrade.$injector) {
+      return
+    }
+
+    this.upgrade.$injector.get('$rootScope').$destroy()
+  }
+
   ngOnInit() {
+    this.destroyAngularJsApp()
     this.upgrade.bootstrap(this.elRef.nativeElement, ['app'], {
       strictDi: true,
     })
-    if (!synced) {
-      setUpLocationSync(this.upgrade)
-      synced = true
-    }
+    setUpLocationSync(this.upgrade)
+  }
+
+  ngOnDestroy() {
+    this.destroyAngularJsApp()
   }
 }
